e9c2d8d4c45940cc9494d3f44340ca68053770ec
Omar Garcia <omar.garciapalencia@gmail.com>
2023-12-05 20:21:39 +1300

    Initial Commit

---
+++ piazza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  piazza
+//
+//  Created by omargarcia on 5/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ piazza/piazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  piazzaApp.swift
+//  piazza
+//
+//  Created by omargarcia on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct piazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
